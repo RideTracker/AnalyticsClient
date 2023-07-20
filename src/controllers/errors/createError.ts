cd4bfc8267623c5b56e77f511cc07198a9e8d564
Nora Söderlund <norasoderlund@icloud.com>
2023-07-20 18:29:30 +0200

    Fixed createError

--- src/controllers/errors/createError.ts
+++ src/controllers/errors/createError.ts
@@ -2,7 +2,7 @@ import Client from "../..";
 import { DefaultResponse } from "../../models/DefaultResponse";
 
 export async function createError(client: Client, error: string, data: string, service: string, environment: string, payload: string): Promise<DefaultResponse> {
-    const url = new URL(`${client.host}/api/errors/create`);
+    const url = new URL(`${client.host}/api/error`);
 
     const body = {
         error,
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
-import { AuthClient, AuthClientToken } from "@ridetracker/authclient";
+import { AuthClient, AuthClientToken, RequestMethod } from "@ridetracker/authclient";
 
 export default class AnalyticsClient extends AuthClient {
     constructor(host: string, token?: AuthClientToken) {
         super("AnalyticsClient-0.9.0", host, token);
+    };
+
+    static async request<T = any>(client: AnalyticsClient, method: RequestMethod, url: URL, initialHeaders?: Record<string, string>, body?: BodyInit | undefined): Promise<T> {
+        return new Promise((resolve, reject) => {
+            super.request(client, method, url, initialHeaders, body).then(async (response) => {
+                if(response.status !== 200)
+                    throw new Error("Unexpected HTTP error, status code " + response.status + " " + response.statusText + "\nBody: " + (await response.text()));
+            
+                const result = await response.json();
+
+                resolve(result);
+            }).catch((error) => {
+                console.error(error);
+
+                reject(error);
+            });
+        });
     };
 };
 
